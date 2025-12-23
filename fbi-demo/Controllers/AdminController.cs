df9669c0caab3199da7690765c9d823c8b0a37b4
Zehavi <ze8544@gmail.com>
2025-12-23 18:17:49 +0200

    ;111

---
+++ fbi-demo/Controllers/AdminController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using FBI.Models;
+using FBI.Services;
+
+namespace FBI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdminController : ControllerBase
+    {
+        public AdminController() { }
+
+        [HttpPost]
+        [Route("[action]")]
+        public ActionResult<String> Login([FromBody] User User)
+        {
+            var dt = DateTime.Now;
+            //var query = $"select * from users where idnumber = @idnumber";
+            if (User.Username != "Wray"
+            || User.Password != $"W{dt.Year}#{dt.Day}!")
+            {
+                return Unauthorized();
+            }
+
+            var claims = new List<Claim>
+            {
+                new Claim("username", User.Username),
+                new Claim("type", "Admin"),
+            };
+
+            var token = FbiTokenService.GetToken(claims);
+
+            return new OkObjectResult(FbiTokenService.WriteToken(token));
+        }
+
+
+        [HttpPost]
+        [Route("[action]")]
+        [Authorize(Policy = "Admin")]
+        public IActionResult GenerateBadge([FromBody] Agent agent)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim("username", agent.Name),
+                new Claim("type", "Agent"),
+                new Claim("ClearanceLevel", agent.ClearanceLevel.ToString()),
+            };
+
+            var token = FbiTokenService.GetToken(claims);
+
+            return new OkObjectResult(FbiTokenService.WriteToken(token));
+        }
+    }
+
+
+
+}
---
+++ fbi-demo/Controllers/AgentController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using FBI.Models;
+
+namespace FBI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize(Policy = "AllUsers")]
+    public class AgentController : ControllerBase
+    {
+        public AgentController() { }
+
+
+        [HttpGet]
+        [Route("[action]")]
+        [Authorize(Policy = "ClearanceLevel1")]
+        public ActionResult<String> AccessPublicFiles()
+        {
+            return new OkObjectResult("Public Files Accessed");
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [Authorize(Policy = "ClearanceLevel2")]
+        public ActionResult<String> AccessClassifiedFiles()
+        {
+            return new OkObjectResult("Classified Files Accessed");
+        }
+    }
+}
